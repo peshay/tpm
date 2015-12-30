359e8d96b894ffee56a5c9cde069c9b5fb4b483b
Andreas Hubert <andreas.hubert@censhare.com>
2015-12-30 17:39:04 +0100

    new v3 version, that allows API auth

--- tpm.py
+++ tpm.py
@@ -6,30 +6,23 @@ for use, please install requests library: pip install requests
 created by Andreas Hubert, censhare AG
 """
 
-__version__ = '2.5'
+__version__ = '3.0'
 
-import json
+import hmac
+import hashlib
+import time
 import requests
 import re
+import json
 
 
-class bcolors:
-
-    """Some colors."""
-
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+class TPMException(Exception):
+    pass
 
 
-class Connection:
+class TpmApi(object):
     """Settings needed for the connection to Team Password Manager."""
-    class ConnectionSetting(Exception):
+    class ConfigError(Exception):
         """To throw Exception based on wrong Settings."""
         def __init__(self, value):
             self.value = value
@@ -37,8 +30,10 @@ class Connection:
         def __str__(self):
             return repr(self.value)
 
-    def __init__(self, api, url, user, password):
+    def __init__(self, api, base_url, kwargs):
         """init thing."""
+
+        # Check if API version and URL is not bullshit
         AllowedAPI = ['v3', 'v4']
         REGEXurl = "^" \
                    "(?:(?:https?)://)" \
@@ -57,358 +52,129 @@ class Connection:
                    "(?:[/?#]\\S*)?" \
                    "$"
         if api in AllowedAPI:
-            apiurl = '/index.php/api/' + api + '/'
+            self.apiurl = '/index.php/api/' + api + '/'
         else:
-            raise ConnectionSetting('API Version not known: %s' % api)
-        self.api = apiurl
-        if re.match(REGEXurl, url):
-            self.url = url
+            raise ConfigError('API Version not known: %s' % api)
+        self.api = self.apiurl
+        if re.match(REGEXurl, base_url):
+            self.url = base_url + self.apiurl
+            self.base_url = base_url
         else:
-            raise ConnectionSetting('Invalid URL: %s' % url)
-        self.user = user
-        self.password = password
+            raise ConfigError('Invalid URL: %s' % url)
+        # set header
+        self.headers = {'Content-Type': 'application/json; charset=utf-8',
+                        'User-Agent': 'tpm.py/' + __version__
+                        }
+        # check kwargs for either keys or user credentials
+        auth1 = False
+        auth2 = False
+        self.private_key = False
+        self.public_key = False
+        self.username = False
+        self.password = False
+        self.unlock_reason = False
+        for key in kwargs:
+            if key == 'private_key':
+                self.private_key = kwargs[key]
+                auth1 = True
+            elif key == 'public_key':
+                self.public_key = kwargs[key]
+                auth2 = True
+            elif key == 'username':
+                self.username = kwargs[key]
+                auth1 = True
+            elif key == 'password':
+                self.password = kwargs[key]
+                auth2 = True
+        if auth1 is False or auth2 is False:
+            raise ConfigError('No authentication specified'
+                              ' (user/password or private/public key)')
 
+    def request(self, path, action, data=''):
+        head = self.base_url
+        if path.startswith(head):
+            path = path[len(head):]
+        if not path.startswith(self.api):
+            path = self.api + path
+        if data:
+            data = json.dumps(data)
+        if self.private_key and self.public_key:
+            timestamp = str(int(time.time()))
+            unhashed = path + timestamp + data
+            hash = hmac.new(str.encode(self.private_key),
+                            msg=unhashed.encode('utf-8'),
+                            digestmod=hashlib.sha256).hexdigest()
+            self.headers['X-Public-Key'] = self.public_key
+            self.headers['X-Request-Hash'] = hash
+            self.headers['X-Request-Timestamp'] = timestamp
+        elif self.username and self.password:
+            auth = requests.auth.HTTPBasicAuth(self.username, self.password)
 
-# disable unsecure SSL warning
-requests.packages.urllib3.disable_warnings()
+        if self.unlock_reason:
+            self.headers['X-Unlock-Reason'] = self.unlock_reason
 
-# set header as team password manager wants them
-header = {'content-type': 'application/json; charset=utf-8'}
-
-# create new empty data object to fill
-global data
-
-
-def HandleRequestsException(e):
-    """Handle Exception from request."""
-    print(bcolors.FAIL + e[0][0])
-    print(e[0][1])
-    print(bcolors.ENDC)
-
-
-def HandleAPIErrors(r):
-    """To handle Errors from TPM API."""
-    # Handle API Errors
-    if r.status_code != 200 and r.status_code != 201 and r.status_code != 204:
-        content = json.load(r.raw)
-        print('%sError:    %s' % (bcolors.FAIL, str(r.status_code)))
-        print('Type:     %s' % content['type'])
-        print('Message:  %s' % content['message'])
-        print('With URL: %s' % r.url + bcolors.ENDC)
-
-
-def checkType(TYPE):
-    """Test if given type is correct."""
-    # check if a valid type was added
-    if TYPE is not 'passwords' \
-       and TYPE is not 'projects' \
-       and TYPE is not 'users' \
-       and TYPE is not 'groups':
-        print("%sconnect type is neither "
-              "'passwords', 'projects', 'users' or 'groups'%s" %
-              (bcolors.WARNING, bcolors.ENDC))
-
-
-def checkTypeEntry(TYPE):
-    """Test if given type is correct."""
-    # check if a valid type was added
-    if TYPE is not 'passwords' \
-       and TYPE is not 'projects':
-        print("%sconnect type is neither "
-              "'passwords' or 'projects'%s" % (bcolors.WARNING, bcolors.ENDC))
-
-
-def get(conn, URL):
-    """Return all results pagewise in list of dictionaries."""
-    data = []
-    # get search results
-    while URL:
-        # try to connect and handle errors
+        url = head + path
         try:
-            r = requests.get(URL, auth=(conn.user, conn.password),
-                             headers=(header), stream=True, verify=False)
-            # Handle API Errors
-            HandleAPIErrors(r)
-            # get results
-            result = json.load(r.raw)
-            # check for rel next link
-            if 'link' in r.headers:
-                RELNEXT = r.links['next']
-                URL = RELNEXT['url']
-            else:
-                URL = ''
+            if action == 'get':
+                self.req = requests.get(url, headers=self.headers, auth=auth,
+                                        verify=False)
+            elif action == 'post':
+                self.req = requests.post(url, headers=self.headers, auth=auth,
+                                         verify=False, data=data)
+            elif action == 'put':
+                self.req = requests.put(url, headers=self.headers, auth=auth,
+                                        verify=False, data=data)
+            elif action == 'delete':
+                self.req = requests.delete(url, headers=self.headers,
+                                           verify=False, auth=auth)
+
+            result = self.req.json()
+            if 'error' in result and result['error']:
+                raise TPMException(result['message'])
+
         except requests.exceptions.RequestException as e:
-            HandleRequestsException(e)
-        # add data pagewise
-        data.extend(result)
-    return data
+            raise TPMException("Connection error for " + str(e))
+
+        except ValueError as e:
+            if self.req.status_code == 403:
+                raise TPMException(url + " forbidden")
+            elif self.req.status_code == 404:
+                raise TPMException(url + " not found")
+            else:
+                raise TPMException(self.req.text)
+
+        return result
+
+    def post(self, path, data):
+        return self.request(path, 'post', data)
+
+    def get(self, path):
+        return self.request(path, 'get')
+
+    def get_collection(self, path):
+        while True:
+            items = self.get(path)
+            req = self.req
+            for item in items:
+                yield item
+            if req.links and req.links['next'] and\
+                    req.links['next']['rel'] == 'next':
+                path = req.links['next']['url']
+            else:
+                break
+
+    def listPasswords(self):
+        data = []
+        for item in self.get_collection('passwords.json'):
+            data.append(item)
+        return data
 
 
-def getSingle(conn, URL):
-    """Return the single results in dictionary."""
-    data = {}
-    # get search results
-    # try to connect and handle errors
-    try:
-        r = requests.get(URL, auth=(conn.user, conn.password),
-                         headers=(header), stream=True, verify=False)
-        # Handle API Errors
-        HandleAPIErrors(r)
-        # get results
-        data.update(json.load(r.raw))
-        # check for rel next link
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
-    return data
+class TpmApiv3(TpmApi):
+    def __init__(self, url, **kwargs):
+        super(TpmApiv3, self).__init__('v3', url, kwargs)
 
 
-def generatePass(conn):
-    """Generate a random password."""
-    # build URL
-    URL = conn.url + conn.api + "generate_password.json"
-    # try to connect and handle errors
-    try:
-        r = requests.get(URL, auth=(conn.user, conn.password),
-                         headers=(header), stream=True, verify=False)
-        # Handle request Errors
-        HandleAPIErrors(r)
-        return json.load(r.raw).get('password')
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
-
-
-def put(conn, URL, DATA):
-    """Update data in TPM."""
-    # convert DATA to JSON
-    JSON = json.dumps(DATA)
-    # try to connect and handle errors
-    try:
-        r = requests.put(URL, data=JSON, auth=(conn.user, conn.password),
-                         headers=(header), stream=True, verify=False)
-        # Handle request Errors
-        HandleAPIErrors(r)
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
-
-
-def post(conn, URL, DATA):
-    """Post data to TPM."""
-    # convert DATA to JSON
-    JSON = json.dumps(DATA)
-    # try to connect and handle errors
-    try:
-        r = requests.post(URL, data=JSON, auth=(conn.user, conn.password),
-                          headers=(header), stream=True, verify=False)
-        # Handle request Errors
-        HandleAPIErrors(r)
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
-    # return ID that has beend created
-    return json.load(r.raw)
-
-
-def getData(conn, TYPE, SEARCHSTRING=''):
-    """Connect to TPM and returns found Entries in a dictionary."""
-    # check if type is password or projects
-    checkType(TYPE)
-    # Search or All
-    if SEARCHSTRING:
-        URL = conn.url + conn.api + TYPE + "/search/" + SEARCHSTRING + ".json"
-    else:
-        URL = conn.url + conn.api + TYPE + ".json"
-    # return data dictionary
-    return get(conn, URL)
-
-
-def getSubProjects(conn, ID):
-    """Get projcets that are subprojects of 'ID'."""
-    if conn.api == '/index.php/api/v4/':
-        URL = conn.url + conn.api + '/projects/' + str(ID) + \
-            '/subprojects.json'
-        # return data dictionary
-        return get(conn, URL)
-    else:
-        print('%sThis functions only works with v4 API.' %
-              (bcolors.FAIL, bcolors.ENDC))
-
-
-def getSubProjectsNewPwd(conn, ID):
-    """Get projcets that are subprojects of 'ID' and shows disabled=true
-    if the Users permissions does not allow to create a new Password in that
-    subproject."""
-    if conn.api == '/index.php/api/v4/':
-        URL = conn.url + conn.api + '/projects/' + str(ID) + \
-            '/subprojects/new_pwd.json'
-        # return data dictionary
-        return get(conn, URL)
-    else:
-        print('%sThis functions only works with v4 API.' %
-              (bcolors.FAIL, bcolors.ENDC))
-
-
-def changeParent(conn, ID, ParentID):
-    """Change the Parent Project of a Project."""
-    if conn.api == '/index.php/api/v4/':
-        URL = conn.url + conn.api + '/projects/' + str(ID) + \
-            '/change_parent.json'
-        DATA = {"parent_id": ParentID}
-        # return data dictionary
-        return put(conn, URL, DATA)
-    else:
-        print('%sThis functions only works with v4 API.' %
-              (bcolors.FAIL, bcolors.ENDC))
-
-
-def getDetailData(conn, TYPE, ID):
-    """Get more Details from an Entry."""
-    # check if type is password or projects
-    checkType(TYPE)
-    URL = conn.url + conn.api + TYPE + "/" + str(ID) + ".json"
-    # return data dictionary
-    return getSingle(conn, URL)
-
-
-def getProjectPasswords(conn, ID):
-    """Get the password entries from a project."""
-    # Build URL
-    URL = conn.url + conn.api + 'projects/' + str(ID) + '/passwords.json'
-    # return data dictionary
-    return get(conn, URL)
-
-
-def getProjectPasswordsCount(conn, ID):
-    """Return the number of password entries from a project."""
-    # Build URL
-    URL = conn.url + conn.api + 'projects/' + str(ID) + '/passwords/count.json'
-    # return data dictionary
-    return getSingle(conn, URL)
-
-
-def getArchived(conn, TYPE):
-    """Connect to TPM and returns Data Object."""
-    # check if type is password or projects
-    checkTypeEntry(TYPE)
-    URL = conn.url + conn.api + TYPE + "/archived.json"
-    # return data dictionary
-    return get(conn, URL)
-
-
-def getFavorite(conn, TYPE):
-    """Connect to TPM and returns Data Object."""
-    # check if type is password or projects
-    checkTypeEntry(TYPE)
-    # build URL
-    URL = conn.url + conn.api + TYPE + "/favorite.json"
-    # return data dictionary
-    return get(conn, URL)
-
-
-def getSecurity(conn, TYPE, ID):
-    """List Users that have Access to a specific entry by ID."""
-    # check if type is password or projects
-    checkTypeEntry(TYPE)
-    # build URL
-    URL = conn.url + conn.api + TYPE + '/' + ID + "/security.json"
-    # return data dictionary
-    return get(conn, URL)
-
-
-def putSecurity(conn, TYPE, ID, DATA):
-    """Update Security Access for an entry."""
-    # check if type is password or projects
-    checkTypeEntry(TYPE)
-    # build URL
-    URL = conn.url + conn.api + TYPE + "/" + ID + "/" + "/security.json"
-    put(conn, URL, DATA)
-
-
-def putCustomFields(conn, ID, DATA):
-    """Update the Custom Fields in TPM."""
-    # build URL
-    URL = conn.url + conn.api + "passwords" + "/" + ID + "/" + \
-                                                         "custom_fields.json"
-    put(conn, URL, DATA)
-
-
-def putData(conn, TYPE, ID, DATA):
-    """Update an entry in TPM."""
-    # check if type is password or projects
-    checkType(TYPE)
-    # build URL
-    URL = conn.url + conn.api + TYPE + "/" + ID + ".json"
-    # update to TPM
-    put(conn, URL, DATA)
-
-
-def lockPassword(conn, ID):
-    """To Lock an entry."""
-    # build URL
-    URL = conn.url + conn.api + "passwords" + "/" + str(ID) + "/" + "lock.json"
-    # try to connect and handle errors
-    try:
-        r = requests.put(URL, auth=(conn.user, conn.password),
-                         headers=(header), stream=True, verify=False)
-        # Handle request Errors
-        HandleAPIErrors(r)
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
-
-
-def unlockPassword(conn, ID, REASON):
-    """To Unlock an entry."""
-    # build URL
-    URL = conn.url + conn.api + "passwords" + "/" + str(ID) + "/" + \
-                                                              "unlock.json"
-    # add Unlock Reason to header
-    header['X-Unlock-Reason'] = REASON
-    # try to connect and handle errors
-    try:
-        r = requests.put(URL, auth=(conn.user, conn.password),
-                         headers=(header), stream=True, verify=False)
-        # Handle request Errors
-        HandleAPIErrors(r)
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
-
-
-def archiveProject(conn, ID):
-    "To Archive a Project."
-    # build URL
-    URL = conn.url + conn.api + 'projects/' + str(ID) + '/archive.json'
-    DATA = {}
-    put(conn, URL, DATA)
-
-
-def unarchiveProject(conn, ID):
-    "To Unarchive a Project."
-    # build URL
-    URL = conn.url + conn.api + 'projects/' + str(ID) + '/unarchive.json'
-    DATA = {}
-    put(conn, URL, DATA)
-
-
-def postData(conn, TYPE, DATA):
-    """Connect to TPM and create an entry."""
-    # check if type is password or projects
-    checkType(TYPE)
-    # build URL
-    URL = conn.url + conn.api + TYPE + ".json"
-    # create entry and return created ID
-    return post(conn, URL, DATA)
-
-
-def deleteData(conn, TYPE, ID):
-    """Connect to TPM and delete an entry."""
-    # check if type is password or projects
-    checkType(TYPE)
-    # create URL to delete entry
-    URL = conn.url + conn.api + TYPE + "/" + str(ID) + ".json"
-
-    # try to connect and handle errors
-    try:
-        r = requests.delete(URL, auth=(conn.user, conn.password),
-                            headers=(header), stream=True, verify=False)
-        # Handle request Errors
-        HandleAPIErrors(r)
-    except requests.exceptions.RequestException as e:
-        HandleRequestsException(e)
+class TpmApiv4(TpmApi):
+    def __init__(self, url, **kwargs):
+        super(TpmApiv4, self).__init__('v4', url, kwargs)
